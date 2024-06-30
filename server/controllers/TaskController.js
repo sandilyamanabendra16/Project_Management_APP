c52b5c60aefc9f1d87ee12cfab2807320ab1524f
sandilyamanabendra16 <81513455+sandilyamanabendra16@users.noreply.github.com>
2024-06-30 17:32:06 +0530

    FIlterissuse v1

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -28,14 +28,14 @@ const getTasks = async (req, res) => {
     startOfWeek.setDate(tomorrow.getDate() - tomorrow.getDay());
     startOfWeek.setHours(0, 0, 0, 0);
 
-    const endOfWeek = new Date(now);
+    const endOfWeek = new Date(tomorrow);
     endOfWeek.setDate(tomorrow.getDate() - tomorrow.getDay() + 6);
     endOfWeek.setHours(23, 59, 59, 999);
 
     query.dueDate = { $gte: startOfWeek, $lte: endOfWeek };
   } else if (filter === 'month') {
-    const startOfMonth = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), 1, 0, 0, 0, 0);
-    const endOfMonth = new Date(tomorrow.getFullYear(), tomorrow.getMonth() + 1, 0, 23, 59, 59, 999);
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0, 0);
+    const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999);
     query.dueDate = { $gte: startOfMonth, $lte: endOfMonth };
   }
 
