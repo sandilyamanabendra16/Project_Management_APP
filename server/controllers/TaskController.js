5148cbbfdfd5507108236ea67d7bc9a7a336e716
sandilyamanabendra16 <81513455+sandilyamanabendra16@users.noreply.github.com>
2024-06-30 15:11:34 +0530

    filter issue v2

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -18,7 +18,7 @@ const getTasks = async (req, res) => {
   tomorrow.setDate(now.getDate());
 
   if (filter === 'today') {
-    const startOfDay = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 0, 0, 0, 0);
+    const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
     const endOfDay = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 23, 59, 59, 999);
     query.dueDate = { $gte: startOfDay, $lte: endOfDay };
   } else if (filter === 'week') {
