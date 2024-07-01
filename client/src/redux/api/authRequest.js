d93f47cbae83b6444e5438a9e49a98a7b5308452
sandilyamanabendra16 <81513455+sandilyamanabendra16@users.noreply.github.com>
2024-07-01 15:36:03 +0530

    Backend Changes

--- client/src/redux/api/authRequest.js
+++ client/src/redux/api/authRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API= axios.create({ baseURL:'https://project-management-app-gqr7.onrender.com' })
+const API= axios.create({ baseURL:'http://localhost:4000' })
 
 export const logIn=(FormData)=>API.post('/auth/login', FormData)
 export const signUp=(FormData)=>API.post('/auth/register', FormData)
